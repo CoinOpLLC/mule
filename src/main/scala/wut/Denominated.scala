fd461492a54fde307798e31a64ccec8b0d8c5d03
ndwade <nicholas.d.wade@gmail.com>
2017-11-07 22:16:24 -0800

    this is basically a gratuitous demo test checkin

--- src/main/scala/wut/Denominated.scala
+++ src/main/scala/wut/Denominated.scala
@@ -23,15 +23,23 @@ import enumeratum._
 // import refined.numeric._
 // import refined.auto._
 
+// TODO: try a typeclass for currency,
+// use an implicit def to turn the type bounds into a set of instances...
+
 sealed trait Currency extends EnumEntry {
-  lazy val jvm: JvmCurrency = JvmCurrency getInstance entryName
+  type C <: Currency
+  def apply[N: Numeric](amount: N): Denominated[N, C]
+  final lazy val jvm: JvmCurrency = JvmCurrency getInstance entryName
 }
 
 object Currency extends Enum[Currency] {
 
   val values = findValues // (sic) macro magicke
 
-  case object USD extends Currency
+  case object USD extends Currency {
+    def apply[N: Numeric](a: N): Denominated[N, USD] = impl.USD[N](a)
+  }
+  type USD = USD.type
   case object EUR extends Currency
   case object JPY extends Currency
   case object AUD extends Currency
@@ -39,6 +47,8 @@ object Currency extends Enum[Currency] {
 
   implicit val eq = Eq.fromUniversalEquals[Currency]
 
+  // this is the problem right here... we capture a type and there's no way to tie it Back
+  // to the correct _dependent type... which is what the methods now produce...
   def of[C <: Currency]: C = ???
 }
 
@@ -49,33 +59,30 @@ sealed trait Denominated[N, +C <: Currency] extends Any { self: Product =>
   final def currency: C = (Currency withName self.productPrefix).asInstanceOf[C]
 }
 
-private[model] final case class USD[N](override val amount: N) extends AnyVal with Denominated[N, Currency.USD.type]
-private[model] final case class EUR[N](override val amount: N) extends AnyVal with Denominated[N, Currency.EUR.type]
-private[model] final case class JPY[N](override val amount: N) extends AnyVal with Denominated[N, Currency.JPY.type]
-private[model] final case class AUD[N](override val amount: N) extends AnyVal with Denominated[N, Currency.AUD.type]
-private[model] final case class XAU[N](override val amount: N) extends AnyVal with Denominated[N, Currency.XAU.type]
-
+package impl {
+  final case class USD[N](val amount: N) extends AnyVal with Denominated[N, Currency.USD]
+  // final case class EUR[N](val amount: N) extends AnyVal with Denominated[N, Currency.EUR.type]
+  // final case class JPY[N](val amount: N) extends AnyVal with Denominated[N, Currency.JPY.type]
+  // final case class AUD[N](val amount: N) extends AnyVal with Denominated[N, Currency.AUD.type]
+  // final case class XAU[N](val amount: N) extends AnyVal with Denominated[N, Currency.XAU.type]
+}
 object Denominated {
+  def apply[N: Numeric, C <: Currency](n: N, c: C): Denominated[N, c.C] = c(n)
   trait Amount[C <: Currency] {
     def apply[N: Numeric]: N => Denominated[N, C]
     def is[N: Numeric](amount: N): Denominated[N, C] = amount |> apply[N]
   }
-  def in[C <: Currency](c: C) = new Amount[C] {
-    override def apply[N: Numeric]: N => Denominated[N, C] =
-      amount =>
-        c match {
-          case Currency.USD => USD(amount)
-          case Currency.EUR => EUR(amount)
-          case Currency.JPY => JPY(amount)
-          case Currency.AUD => AUD(amount)
-          case Currency.XAU => XAU(amount)
-      }
+  def in[C <: Currency](currency: C) = new Amount[currency.C] {
+    override def apply[N: Numeric]: N => Denominated[N, currency.C] =
+      amount => currency(amount)
   }
 
+  // def [N: Numeric, C <: Currency]
   def unapply[N: Numeric, C <: Currency](dn: Denominated[N, C]): Option[(N, C)] = (dn.amount, dn.currency).some
 
   private type CaseDenominated[N, C <: Currency] = Denominated[N, C] with Product
-  implicit def eq[N: Numeric, C <: Currency, CD <: CaseDenominated[N, C]]: Eq[CD] = Eq.fromUniversalEquals[CD]
+  implicit def eq[N: Numeric]: Eq[N] = Eq.fromUniversalEquals[N]
+  implicit def eq[N: Numeric, C <: Currency]: Eq[Denominated[N, C]] = Eq.by(unapply(_))
 
   implicit def monoid[N: Numeric, C <: Currency] = new Monoid[Denominated[N, C]] {
     val N = implicitly[Numeric[N]]
@@ -83,12 +90,15 @@ object Denominated {
     override def combine(a: CD, b: CD): CD = Denominated in Currency.of[C] is N.plus(a.amount, b.amount)
     override lazy val empty: CD            = Denominated in Currency.of[C] is N.zero
   }
+
 }
 
 object Examples {
-  val dollarMonoid = Monoid[Denominated[BigDecimal, Currency.USD.type]]
+  val dollarMonoid = Monoid[BigDecimal Denominated Currency.USD.type]
 
   val bux = Denominated in Currency.USD is 555.55
-  // val bux       = 555 |> buckMaker
+  val buxMaker = (Denominated in Currency.USD)[Double]
+  val moarBux = 555.55 |> buxMaker
+  bux === moarBux |> assert
 
 }
