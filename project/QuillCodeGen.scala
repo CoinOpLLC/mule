58b1e59a7196819b7f976b42ec2fcd1d85696ccd
ndwade <nicholas.d.wade@gmail.com>
2017-12-20 06:49:16 -0800

    doesn't build. must sleep.

--- project/QuillCodeGen.scala
+++ project/QuillCodeGen.scala
@@ -1,6 +1,8 @@
 package io.deftrade.sbt
 
 import java.io.File
+import java.nio.file.{Files, Paths}
+import java.sql.{ Connection, DriverManager, ResultSet }
 /**
  *  This customizes the Slick code generator. We only do simple name mappings.
  *  For a more advanced example see https://github.com/cvogt/slick-presentation/tree/scala-exchange-2013
@@ -8,6 +10,18 @@ import java.io.File
 object QuillCodeGen {
 
   type EnumModel = Vector[(String, String)] // enum -> value
+
+  val TABLE_NAME = "TABLE_NAME"
+  val COLUMN_NAME = "COLUMN_NAME"
+  val TYPE_NAME = "TYPE_NAME"
+  val NULLABLE = "NULLABLE"
+  val PK_NAME = "pk_name"
+  val FK_TABLE_NAME = "fktable_name"
+  val FK_COLUMN_NAME = "fkcolumn_name"
+  val PK_TABLE_NAME = "pktable_name"
+  val PK_COLUMN_NAME = "pkcolumn_name"
+
+  val scalaFileName = "GeneratedQuillCode.scala"
 
   val defaultTypeMap = Map(
     "int4" -> "Int",
@@ -27,17 +41,6 @@ object QuillCodeGen {
     "jsonb" -> "me.fix.Json"
   )
 
-  val scalaFileName = "GeneratedQuillCode.scala"
-
-  val TABLE_NAME = "TABLE_NAME"
-  val COLUMN_NAME = "COLUMN_NAME"
-  val TYPE_NAME = "TYPE_NAME"
-  val NULLABLE = "NULLABLE"
-  val PK_NAME = "pk_name"
-  val FK_TABLE_NAME = "fktable_name"
-  val FK_COLUMN_NAME = "fkcolumn_name"
-  val PK_TABLE_NAME = "pktable_name"
-  val PK_COLUMN_NAME = "pkcolumn_name"
 
   def apply(
     driver: String,
@@ -47,7 +50,7 @@ object QuillCodeGen {
     folder: File, // output directory for generated source
     pkg: String,
     schema: String = "public",
-    imports: String = """""",
+    imports: String = "",
     typeMap: Map[String, String] = defaultTypeMap,
     excludedTables: Set[String] = Set("schema_version"),
     namingStrategy: ReverseNamingStrategy = ReverseSnakeCase
@@ -56,6 +59,7 @@ object QuillCodeGen {
 
     val file = ??? // folder / scalaFileName
     val logstream = System.err  // FIXME lol no... or maybe?!
+    val codeStyle = ScalafmtConfig.defaultWithAlign copy (maxColumn = 120)
 
     logstream println a"Starting output generation for $file..."
 
@@ -65,181 +69,228 @@ object QuillCodeGen {
 
     val db: Connection = DriverManager.getConnection(url, user, password)
 
-    // FIXME: CURSOR start again here...
+    val foreignKeys: Set[ForeginKey] = {
 
-    val codegen = Codegen(codegenOptions)
-    val plainForeignKeys = codegen.getForeignKeys(db)
+      // construct raw `ForeignKey`s from the metadata `ResultSet`
+      val unresolvedFKs = for {
+        tRs   <- results(db.getMetaData getTables       (null, schema, "%", Array("TABLE")))
+        fkRs  <- results(db.getMetaData getExportedKeys (null, schema, tRs getString TABLE_NAME))
+      } yield
+        ForeignKey (
+          from = SimpleColumn(fkRs getString FK_TABLE_NAME, fkRs getString FK_COLUMN_NAME),
+          to   = SimpleColumn(fkRs getString PK_TABLE_NAME, fkRs getString PK_COLUMN_NAME)
+        )
 
-    val foreignKeys = plainForeignKeys.map { fk =>
-
-      def resolve(col: codegen.SimpleColumn): codegen.SimpleColumn = {
-        plainForeignKeys.find(f => f.from == col).map(_.to).getOrElse(col)
-      }
-
-      codegen.ForeignKey(fk.from, resolve(fk.to))
-
-    def results(resultSet: ResultSet): Iterator[ResultSet] = {
-      new Iterator[ResultSet] {
-        def hasNext = resultSet.next()
-        def next() = resultSet
-      }
+      // "resolve" by following references (?!)
+      for {
+        xfk <- unresolvedFKs
+        yfk <- unresolvedFKs
+      } yield if (xfk.to == yfk.from) ForeignKey(from = xfk.from, to = yfk.to) else xfk
     }
 
-    def getForeignKeys(db: Connection): Set[ForeignKey] = {
-      val allTables =
-        results(db.getMetaData.getTables(null, options.schema, "%", Array("TABLE"))).map { row =>
-          row.getString(TABLE_NAME)
+
+    val tables: Seq[Table] = {
+
+      val tableResultSets =
+        results(db.getMetaData getTables (null, options.schema, "%", Array("TABLE")).toVector
+
+      for {
+        tRs <- tableResultSets if !(excludedTables contains (tRs getString TABLE_NAME))
+      } yield {
+        val tableName = tRs getString TABLE_NAME
+        val columns = {
+
+          val pkNames = Set.empty[String] ++
+            db.getMetaData getPrimaryKeys (null, null, tableName) map { _ getString COLUMN_NAME }
+
+          for {
+            colRs <- results(db.getMetaData getColumns (null, schema, tableName, null)).toSeq
+          } yield {
+            val colName = colRs getString COLUMN_NAME
+            val sqlType = (colRs getString TYPE_NAME).toLowerCase
+            val nullable = colRs getBoolean NULLABLE,
+            val sc = SimpleColumn(tableName, colName)
+            val ref = foreignKeys find (_.from == sc) map (_.to)
+            (typeMap get sqlType).fold(Left(sqlType)) { scalaType =>
+              Right(Column(tableName, colName, scalaType, nullable, pkNames contains colName, ref))
+            }
+          }
         }
-
-      allTables.flatMap { table =>
-        val foreignKeys = db.getMetaData.getExportedKeys(null, options.schema, table)
-        results(foreignKeys).map { row =>
-          ForeignKey(
-            from = SimpleColumn(
-              tableName = row.getString(FK_TABLE_NAME),
-              columnName = row.getString(FK_COLUMN_NAME)
-            ),
-            to = SimpleColumn(
-              tableName = row.getString(PK_TABLE_NAME),
-              columnName = row.getString(PK_COLUMN_NAME)
-            )
-          )
-        }
-      }.toSet
-    }
-
-    def warn(msg: String): Unit = {
-      System.err.println(s"[${Console.YELLOW}warn${Console.RESET}] $msg")
-    }
-
-    def getTables(db: Connection, foreignKeys: Set[ForeignKey]): Seq[Table] = {
-      val rs: ResultSet =
-        db.getMetaData.getTables(null, options.schema, "%", Array("TABLE"))
-      results(rs).flatMap { row =>
-        val name = row.getString(TABLE_NAME)
-        if (!excludedTables.contains(name)) {
-          val columns = getColumns(db, name, foreignKeys)
-          val mappedColumns = columns.filter(_.isRight).map(_.right.get)
-          val unmappedColumns = columns.filter(_.isLeft).map(_.left.get)
-          if (unmappedColumns.nonEmpty)
-            warn(s"The following columns from table $name need a mapping: $unmappedColumns")
-          Some(Table(
-            name,
-            mappedColumns
-          ))
-        } else {
-          None
-        }
-      }.toVector
-    }
-    def getColumns(db: Connection,
-                   tableName: String,
-                   foreignKeys: Set[ForeignKey]): Seq[Either[String, Column]] = {
-      val primaryKeys = getPrimaryKeys(db, tableName)
-      val cols =
-        db.getMetaData.getColumns(null, options.schema, tableName, null)
-      results(cols).map { row =>
-        val colName = cols.getString(COLUMN_NAME)
-        val simpleColumn = SimpleColumn(tableName, colName)
-        val ref = foreignKeys.find(_.from == simpleColumn).map(_.to)
-
-        val typ = cols.getString(TYPE_NAME).toLowerCase
-        typeMap.get(typ).map { scalaType =>
-          Right(Column(
-            tableName,
-            colName,
-            scalaType,
-            cols.getBoolean(NULLABLE),
-            primaryKeys contains cols.getString(COLUMN_NAME),
-            ref
-          ))
-        }.getOrElse(Left(typ))
-      }.toVector
-    }
-
-    def getPrimaryKeys(db: Connection, tableName: String): Set[String] = {
-      val sb = Set.newBuilder[String]
-      val primaryKeys = db.getMetaData.getPrimaryKeys(null, null, tableName)
-      while (primaryKeys.next()) {
-        sb += primaryKeys.getString(COLUMN_NAME)
+        columns collect { case Left(colName) => warn(s"UNMAPPED: table: $table col $colName") }
+        Table(tableName, columns collect { case Right(col) => col })
       }
-      sb.result()
     }
 
-    def tables2code(tables: Seq[Table],
-                    namingStrategy: ReverseNamingStrategy,
-                    options: CodegenOptions) = {
-      val body = tables.map(_.toCode).mkString("\n\n")
-      s"""|package ${options.`package`}
-          |${options.imports}
+    val code =
+      s"""|package ${pkg}
+          |${imports}
           |
-          |//noinspection ScalaStyle
           |object Tables {
-          |$body
+          |${tables map (_.toCode) mkString "
+          |
+          |"}
           |}
-       """.stripMargin
+       """.stripMargin.getBytes
+
+    Files.write(Paths get (file.toURI), code)
+    println(s"Done! Wrote to $uri (${System.currentTimeMillis() - startTime}ms)")
+    db.close()
+
+  }
+
+  def results(resultSet: ResultSet): Iterator[ResultSet] =
+    new Iterator[ResultSet] {
+      def hasNext = resultSet.next()
+      def next() = resultSet
     }
 
-    case class ForeignKey(from: SimpleColumn, to: SimpleColumn)
 
-    case class SimpleColumn(tableName: String, columnName: String) {
-      def toType =
-        s"${namingStrategy.table(tableName)}.${namingStrategy.table(columnName)}"
+  def warn(msg: String): Unit = {
+    System.err.println(s"[${Console.YELLOW}warn${Console.RESET}] $msg")
+  }
+
+  def debugPrintColumnLabels(rs: ResultSet): Unit =
+    for (i = 1 to rs.getMetaData.getColumnCount) {
+      println(rs.getMetaData getColumnLabel i)
     }
+}
 
-    case class Column(tableName: String,
-                      columnName: String,
-                      scalaType: String,
-                      nullable: Boolean,
-                      isPrimaryKey: Boolean,
-                      references: Option[SimpleColumn]) {
-      def scalaOptionType = makeOption(scalaType)
-
-      def makeOption(typ: String): String = {
-        if (nullable) s"Option[$typ]"
-        else typ
+case class Table(name: String, columns: Seq[Column]) {
+  def toCode: String = {
+    val scalaName = namingStrategy.table(name)
+    val args = columns.map(_.toArg(namingStrategy, scalaName)).mkString(", ")
+    val applyArgs = columns.map { column =>
+      s"${namingStrategy.column(column.columnName)}: ${column.scalaOptionType}"
+    }.mkString(", ")
+    val applyArgNames = columns.map { column =>
+      val typName = if (column.references.nonEmpty) {
+        column.toType
+      } else {
+        namingStrategy.table(column.columnName)
       }
-
-      def toType: String = this.toSimple.toType
-
-      def toArg(namingStrategy: ReverseNamingStrategy, tableName: String): String = {
-        s"${namingStrategy.column(columnName)}: ${makeOption(this.toType)}"
+      if (column.nullable) {
+        s"${namingStrategy.column(column.columnName)}.map($typName.apply)"
+      } else {
+        s"$typName(${namingStrategy.column(column.columnName)})"
       }
+    }.mkString(", ")
+    val classes =
+      columns.withFilter(_.references.isEmpty).map(_.toClass).mkString("\n")
 
-      def toSimple = references.getOrElse(SimpleColumn(tableName, columnName))
-
-      def toClass: String = {
-        s"case class ${namingStrategy.table(columnName)}(value: $scalaType) extends AnyVal"
-      }
+    s"""|/**
+        |  * $scalaName
+        |  */
+        |case class $scalaName($args)
+        |object $scalaName {
+        |  def create($applyArgs): $scalaName = {
+        |    $scalaName($applyArgNames)
+        |  }
+        |$classes
+        |}""".stripMargin
   }
 }
 
-object Codegen extends CaseApp[CodegenOptions] {
+case class ForeignKey(from: SimpleColumn, to: SimpleColumn)
+
+case class SimpleColumn(tableName: String, columnName: String) {
+  def toType = s"${namingStrategy table tableName}.${namingStrategy table columnName}"
+}
+
+case class Column(tableName: String,
+                  columnName: String,
+                  scalaType: String,
+                  nullable: Boolean,
+                  isPrimaryKey: Boolean,
+                  references: Option[SimpleColumn]) {
+  def scalaOptionType = makeOption(scalaType)
+
+  def makeOption(typ: String): String = if (nullable) s"Option[$typ]" else typ
 
 
-  def debugPrintColumnLabels(rs: ResultSet): Unit = {
-    (1 to rs.getMetaData.getColumnCount).foreach { i =>
-      println(i -> rs.getMetaData.getColumnLabel(i))
-    }
+  def toType: String = this.toSimple.toType
+
+  def toArg(namingStrategy: ReverseNamingStrategy, tableName: String): String = {
+    s"${namingStrategy.column(columnName)}: ${makeOption(this.toType)}"
   }
 
-    val tables = codegen.getTables(db, foreignKeys)
-    val generatedCode = codegen.tables2code(tables, SnakeCaseReverse, codegenOptions)
-    val codeStyle = ScalafmtConfig.defaultWithAlign.copy(maxColumn = 120)
-    val code = Scalafmt.format(generatedCode, style = codeStyle) match {
-      case Formatted.Success(x) => x
-      case _ => generatedCode
-    }
-    codegenOptions.file match {
-      case Some(uri) =>
-        Files.write(Paths.get(new File(uri).toURI), code.getBytes)
-        println(
-          s"Done! Wrote to $uri (${System.currentTimeMillis() - startTime}ms)")
-      case _ =>
-        logstream.println(code)
-    }
-    db.close()
+  def toSimple = references.getOrElse(SimpleColumn(tableName, columnName))
+
+  def toClass: String = {
+    s"case class ${namingStrategy.table(columnName)}(value: $scalaType) extends AnyVal"
   }
+}
+
+// ---
+
+trait ReverseNamingStrategy extends io.getquill.ReverseNamingStrategy
+
+object ReverseSnakeCase extends ReverseNamingStrategy {
+
+  import CamelCaser.{rehump, decap}
+
+  override def column(s: String): String = decap(rehump(s))
+  override def default(s: String): String = rehump(s)
+}
+
+object ReverseEscapingSnakeCase extends ReverseNamingStrategy {
+
+  import CamelCaser.{rehump, decap, escape}
+
+  override def column(s: String): String = escape(decap(rehump(s)))
+  override def default(s: String): String = rehump(s)
+}
+
+object CamelCaser {
+
+  def rehump(s: String): String = (s.toLowerCase split "_" map capitalize).mkString
+  def decap(s: String): String = s.head.toLower +: s.tail
+  def escape(s: String): String = if (reservedWords contains s) s"`$s`" else s
+
+  private def capitalize(s: String): String = s match {
+    case "" => "_"
+    case s => s.capitalize
+  }
+
+  private val reservedWords = Set(
+    "abstract", "case",
+      "catch",
+      "class",
+      "def",
+      "do",
+      "else",
+      "extends",
+      "false",
+      "final",
+      "finally",
+      "for",
+      "forSome",
+      "if",
+      "implicit",
+      "import",
+      "lazy",
+      "match",
+      "new",
+      "null",
+      "object",
+      "override",
+      "package",
+      "private",
+      "protected",
+      "return",
+      "sealed",
+      "super",
+      "this",
+      "throw",
+      "trait",
+      "try",
+      "true",
+      "type",
+      "val",
+      "var",
+      "while",
+      "with",
+      "yield",
+
 }
 
 
