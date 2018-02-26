6150607f39f717dc2df645bf334b1a95ce89cf2b
ndwade <nicholas.d.wade@gmail.com>
2018-02-26 05:17:42 -0800

    testing Moneta

--- wip/src/main/scala/io/deftrade/wip/Denominated.scala
+++ wip/src/main/scala/io/deftrade/wip/Denominated.scala
@@ -21,9 +21,9 @@ import enumeratum._
 // import enumeratum.values._
 
 //
-import eu.timepit.refined
-import refined.api.Refined
-import refined.W
+// import eu.timepit.refined
+// import refined.api.Refined
+// import refined.W
 
 import cats.Show
 // import refined.collection._
@@ -35,13 +35,14 @@ import cats.Show
    requirements for currency / money:
    - take inspiration from `squants.market`
    - separate types for each currency (no addition across currencies! domain modeling dictates!)
-   - minimal (value classes on [N: Numeric])
-   - summon an implicit Denomination object given a currency amount instance
+   - minimal to no overhead (value classes on [N: Numeric])
+    - can summon implicit Moneta[Currency] typeclass instance given a currency instance
    - `enumeratum` lib for denominations (supported currencies)
    - exploit java Currency support
    - abstract over currencies for single implicit `Monoid` function
-   - denomination enum as factory pattern - can use to make currency
-
+   - moneta enum as factory pattern - can use to make currency
+    (Turns out, this runs into limits to abstraction.)
+   - no dependencies other than algebra
 
    todo: treatment of what's expected to be imported implicitly
 
@@ -78,13 +79,13 @@ object PhantomTypePerCurrency {
       override lazy val empty: MNY              = Money(implicitly[Fractional[N]].zero)
     }
 
-    implicit def showMoney[C <: Currency: Denomination, N: Fractional]: Show[Money[N, C]] =
+    implicit def showMoney[C <: Currency: Moneta, N: Fractional]: Show[Money[N, C]] =
       Show show (Format apply _)
 
     object Format {
       val flags = """#,(""" // decimal-separator, grouping-separators, parens-for-negative
-      def apply[N: Fractional, C <: Currency: Denomination](m: Money[N, C]): String = {
-        val DC   = Denomination[C]
+      def apply[N: Fractional, C <: Currency: Moneta](m: Money[N, C]): String = {
+        val DC   = Moneta[C]
         val fmt  = s"%${flags}.${DC.fractionDigits}f"
         val sfmt = if (implicitly[Fractional[N]].signum(m.amount) < 0) fmt else s" $fmt "
         s"${DC.code} ${m.amount formatted sfmt}"
@@ -95,47 +96,51 @@ object PhantomTypePerCurrency {
   sealed trait Currency
   sealed trait Cross[CA <: Currency, CB <: Currency]
 
-  sealed trait DenominationBase extends EnumEntry { denomination =>
+  private[wip] sealed trait BaseMoneta extends EnumEntry { moneta =>
 
-    type CurrencyType <: Currency
+    // type CurrencyType <: Currency
+    // def apply[N: Fractional](n: N): Money[N, CurrencyType]
+    final lazy val jc = java.util.Currency getInstance moneta.entryName
 
-    def apply[N: Fractional](n: N): Money[N, CurrencyType]
+    final def code: String        = jc.getCurrencyCode
+    final def numericCode: Int    = jc.getNumericCode
+    final def fractionDigits: Int = jc.getDefaultFractionDigits
+    final def displayName: String = jc.getDisplayName
+    final def symbol: String      = jc.getSymbol
 
-    def code: String        = jc.getCurrencyCode
-    def numericCode: Int    = jc.getNumericCode
-    def fractionDigits: Int = jc.getDefaultFractionDigits
-    def displayName: String = jc.getDisplayName
-    def symbol: String      = jc.getSymbol
-
-    private lazy val jc = java.util.Currency getInstance denomination.entryName
   }
 
-  sealed trait Denomination[C <: Currency] extends DenominationBase {
+  sealed trait Moneta[C <: Currency] extends BaseMoneta {
 
-    final type CurrencyType = C
-    def apply[N: Fractional](n: N)                              = Money[N, C](n)
-    def /[CB <: Currency](base: Denomination[CB]): Cross[C, CB] = new Cross[C, CB] {}
+    // final type CurrencyType = C
+    def apply[N: Fractional](n: N)                        = Money[N, C](n)
+    def /[CB <: Currency](base: Moneta[CB]): Cross[C, CB] = new Cross[C, CB] {}
   }
 
-  object Denomination extends Enum[DenominationBase] {
+  object Moneta extends Enum[BaseMoneta] {
 
-    def apply[C <: Currency: Denomination]: Denomination[C] = implicitly
+    def apply[C <: Currency: Moneta]: Moneta[C] = implicitly
 
     val values = findValues
 
-    sealed trait USD    extends Currency
-    implicit object USD extends Currency with Denomination[USD]
+    sealed trait USD extends Currency
+    case object USD  extends Currency with Moneta[USD]
 
-    sealed trait EUR    extends Currency
-    implicit object EUR extends Denomination[EUR]
+    sealed trait EUR extends Currency
+    case object EUR  extends Moneta[EUR]
 
   }
+
+  import Moneta._
+
+  implicit val denominationUsd: Moneta[USD] = USD
+  implicit val denominationEur: Moneta[EUR] = EUR
 }
 
 object ClassPerCurrency {
   // design 2:
-  // make each Denomination a separate case class
-  // gets hung up on `import Denomination._`, which imports all the verboten `apply` instances
+  // make each Moneta a separate case class
+  // gets hung up on `import Moneta._`, which imports all the verboten `apply` instances
   // without the necessary `Fractional` context bound on `N`. And so overloading can't resolve.
   // -> and so devolves back to the same need for a Show[Currency] class as phantom type
   // (design 1)
@@ -143,15 +148,20 @@ object ClassPerCurrency {
   import scala.language.higherKinds
 
   sealed trait Currency[N] extends Any {
-    def code: String
+
     def amount: N
+
+    final def code: String = { // principle of least ghastliness
+      val cs = getClass.toString
+      cs substring (cs lastIndexOf '$') + 1
+    }
     final override def toString: String = s"$code(${amount.toString})"
   }
 
   object Currency {
-    implicit class CurrencyOps[C[?] <: Currency[?]: Denomination, N: Fractional](lhs: C[N]) {
+    implicit class CurrencyOps[C[?] <: Currency[?]: Moneta, N: Fractional](lhs: C[N]) {
       def N: Fractional[N]     = implicitly
-      def DC: Denomination[C]  = implicitly
+      def DC: Moneta[C]        = implicitly
       def +(rhs: C[N]): C[N]   = DC apply (N plus (lhs.amount, rhs.amount))
       def /(rhs: C[N]): Double = (N toDouble lhs.amount) / (N toDouble rhs.amount)
       def *(scale: N): C[N]    = DC apply (N times (scale, lhs.amount))
@@ -159,88 +169,77 @@ object ClassPerCurrency {
     }
   }
 
-  sealed trait DenominationBase extends EnumEntry { denomination =>
+  private[wip] sealed trait BaseMoneta extends EnumEntry { moneta =>
 
-    final lazy val jc = java.util.Currency getInstance denomination.entryName
+    final lazy val jc = java.util.Currency getInstance moneta.entryName
 
     final def code: String        = jc.getCurrencyCode
     final def numericCode: Int    = jc.getNumericCode
     final def fractionDigits: Int = jc.getDefaultFractionDigits
     final def displayName: String = jc.getDisplayName
     final def symbol: String      = jc.getSymbol
+
   }
 
-  sealed trait Denomination[C[?] <: Currency[?]] extends DenominationBase {
+  sealed trait Moneta[C[?] <: Currency[?]] extends BaseMoneta {
+
     def apply[N: Fractional](n: N): C[N]
-    final def /[C2[?] <: Currency[?]](base: Denomination[C2]): Denomination.Cross =
-      new Denomination.Cross {}
+
+    final def /[C2[?] <: Currency[?]](base: Moneta[C2]): Moneta.Cross[C, C2] =
+      new Moneta.Cross[C, C2] {}
   }
 
-  object Denomination extends Enum[DenominationBase] {
+  object Moneta extends Enum[BaseMoneta] {
     import cats.kernel.{ Monoid, Order }
 
-    def apply[C[?] <: Currency[?]: Denomination]: Denomination[C] = implicitly
+    def apply[C[?] <: Currency[?]: Moneta]: Moneta[C] = implicitly
 
     def values = findValues
 
-    trait Cross
+    trait Cross[Cn[?] <: Currency[?], Cd[?] <: Currency[?]]
 
-    final class USD[N] private (val amount: N) extends AnyVal with Currency[N] {
-      def code = "USD"
-    }
-    implicit object USD extends Denomination[USD] {
+    final class USD[N] private (val amount: N) extends AnyVal with Currency[N]
+    case object USD extends Moneta[USD] {
       def apply[N: Fractional](n: N) = new USD(n)
     }
 
-    final class EUR[N] private (val amount: N) extends AnyVal with Currency[N] {
-      def code = "EUR"
-    }
-    implicit object EUR extends Denomination[EUR] {
+    final class EUR[N] private (val amount: N) extends AnyVal with Currency[N]
+    case object EUR extends Moneta[EUR] {
       def apply[N: Fractional](n: N) = new EUR(n)
     }
 
     implicit def order[N: Fractional] = Order.fromOrdering[N]
+    implicit def orderMoney[N: Order, C[?] <: Currency[?]: Moneta]: Order[C[N]] =
+      Order by (_.amount)
 
-    implicit def monoid[N: Fractional, C[?] <: Currency[?]: Denomination] = new Monoid[C[N]] {
+    implicit def monoid[N: Fractional, C[?] <: Currency[?]: Moneta] = new Monoid[C[N]] {
       val N: Fractional[N]                         = implicitly
-      val DC: Denomination[C]                      = implicitly
+      val DC: Moneta[C]                            = implicitly
       override def combine(a: C[N], b: C[N]): C[N] = DC apply (N plus (a.amount, b.amount))
       override lazy val empty: C[N]                = DC apply (N.zero)
     }
   }
-  implicit def showMoney[C[?] <: Currency[?]: Denomination, N: Fractional]: Show[C[N]] =
+  implicit def showMoney[C[?] <: Currency[?]: Moneta, N: Fractional]: Show[C[N]] =
     Show show (Format apply _)
 
   object Format {
     val flags = """#,(""" // decimal-separator, grouping-separators, parens-for-negative
-    def apply[N: Fractional, C[?] <: Currency[?]: Denomination](m: C[N]): String = {
-      val DC   = Denomination[C]
+    def apply[N: Fractional, C[?] <: Currency[?]: Moneta](m: C[N]): String = {
+      val DC   = Moneta[C]
       val fmt  = s"%${flags}.${DC.fractionDigits}f"
       val sfmt = if (implicitly[Fractional[N]].signum(m.amount) < 0) fmt else s" $fmt "
       s"${DC.code} ${m.amount formatted sfmt}"
     }
+
   }
-}
 
-object Examples {
+  import Moneta._
 
-  // import wip.PhantomTypePerCurrency._, Denomination._
-  import wip.ClassPerCurrency._, Denomination._
-
-  val eur = EUR // Denomination withName "EUR"
-  // val eurF  = (d: Double) => eur(d)
-  val eurF  = eur[Double] _
-  val eur20 = 20.0 |> eurF
-
-  val d20 = USD(20.00)
-  val d21 = USD(21.00)
-  val e20 = EUR(20.00)
-
-  // def funge[C <: Currency](den: Denomination[C]): Money[Double, C] = den(19.47)
-  // def funge[C[?] <: Currency[?]](den: Denomination[C]): C[Double] = den(19.47)
+  implicit val denominationUsd: Moneta[USD] = USD
+  implicit val denominationEur: Moneta[EUR] = EUR
 
 }
 
-private[wip] object UglyTypeDefs {
-  type Code = String Refined refined.string.MatchesRegex[W.`"[A-Z]{3}"`.T]
-}
+// private[wip] object UglyTypeDefs {
+//   type Code = String Refined refined.string.MatchesRegex[W.`"[A-Z]{3}"`.T]
+// }
---
+++ wip/src/test/scala/io/deftrade/wip/MoneySpec.scala
@@ -0,0 +1,65 @@
+package io.deftrade
+package wip
+
+import scala.language.higherKinds
+import cats.syntax.show._
+
+import org.scalatest.{ prop, FlatSpec, PropSpec }, prop.GeneratorDrivenPropertyChecks
+
+import ClassPerCurrency.{ Currency, Moneta }
+// import PhantomTypePerCurrency._
+import Moneta._
+import Currency._
+
+class MoneySpec extends FlatSpec {
+
+  "Money" should "be created elastically" in {
+
+    import cats.syntax.order.{ catsSyntaxOrder, catsSyntaxPartialOrder }
+
+    val eur = EUR // Moneta withName "EUR"
+    // val eurF  = (d: Double) => eur(d)
+    val eurF  = eur[Double] _
+    val eur20 = 20.0 |> eurF
+    val e20   = EUR(20.00)
+    assert(eur20 === e20)
+
+    type Dollar = USD[Double]
+
+    val d20: Dollar = USD(20.00)
+    val d21: Dollar = USD(21.00)
+
+    assert(d20 !== d21)
+    assert(d20 < d21)
+    assert((d20 compare d21) === -1)
+
+    // def funge[C <: Currency](den: Moneta[C]): Money[Double, C] = den(19.47)
+    def funge[C[?] <: Currency[?]](den: Moneta[C]): C[Double] = den(19.47)
+
+    assert(USD(19.47) === funge[USD](USD))
+
+    val usd               = USD
+    val buck: USD[Double] = usd(1.0)
+    assert(buck === USD(1.0))
+    assert(buck + buck === USD(2.0))
+
+    assert(buck.show === "USD  1.00 ")
+
+  }
+}
+class MoneyPropSpec extends PropSpec with GeneratorDrivenPropertyChecks {
+// with TableDrivenPropertyChecks {
+  property("unit is as unit does") {
+    forAll { ewie: Unit =>
+      assert(ewie === (()))
+    }
+  }
+  property("doubles gonna double") {
+    forAll { xs: List[Double] =>
+      whenever(xs.nonEmpty) {
+        import scala.language.postfixOps
+        assert(math.sqrt(xs map (x => x * x) sum) >= 0)
+      }
+    }
+  }
+}
