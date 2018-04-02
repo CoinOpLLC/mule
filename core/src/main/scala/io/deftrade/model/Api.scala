11fb2a1fe3c1484d828898861cbe858d66d84064
ndwade <nicholas.d.wade@gmail.com>
2018-04-02 02:01:37 -0700

    fleshing out Execution and rearranging

--- core/src/main/scala/io/deftrade/model/Api.scala
+++ core/src/main/scala/io/deftrade/model/Api.scala
@@ -18,6 +18,7 @@ package io.deftrade
 package model
 
 import io.deftrade.money.{ Financial, Monetary }, Monetary._
+import io.deftrade.time._
 
 import cats.{ Eq, Monoid }
 import cats.implicits._
@@ -32,13 +33,30 @@ import refined.numeric._
 import enumeratum._
 // import enumeratum.values._
 
+import impl._
+
 /**
   * This shall be the law of the Api: A `type Foo` may not depend on a `type FooId`.
   * This shall be another: only member names whose appearence cannot be helped may appear here.
   */
 abstract class Api[MonetaryAmount: Financial, Quantity: Financial] {
 
-  import impl._
+  /** Domain specific tools for dealing with `Quantity`s */
+  object Quantity {
+
+    /**  Stable values useful for pattern matching. */
+    val Zero: Quantity = Financial[Quantity].zero
+
+    /**  Stable values useful for pattern matching. */
+    val One: Quantity = Financial[Quantity].one
+
+    /** Extractors and (any) other tools for dealing with integral quantities. */
+    object Integral {
+
+      def unapply[N: Integral](q: Quantity): Option[N] = ???
+      // TODO: etc
+    }
+  }
 
   sealed trait Role extends EnumEntry
   object Role extends Enum[Role] {
@@ -64,65 +82,84 @@ abstract class Api[MonetaryAmount: Financial, Quantity: Financial] {
     lazy val values: IndexedSeq[Role] = findValues
   }
 
-  private[model] sealed trait Id[T] extends Any { def id: T }
+  trait Person
+  trait Corporation
 
-  type Entity   = Nothing // Yet
+  type Entity   = Either[Corporation, Person]
   type EntityId = Long Refined Interval.Closed[W.`100100100100`, W.`999999999999`]
   // type EntityId = OpaqueId[Long, Entity]
   implicit def EntityIddHasEq = Eq.fromUniversalEquals[EntityId]
 
-  type Asset   = Nothing // yet
+  trait Stawks
+  trait Bawnds
+
+  type Asset   = Either[Stawks, Bawnds]
   type AssetId = OpaqueId[Long, Asset]
   implicit def AssetIdHasEq = Eq.fromUniversalEquals[AssetId]
 
-  type Position = (AssetId, Quantity)
-  type Folio    = Map[AssetId, Quantity]
-  type FolioId  = OpaqueId[Long, Folio]
-  implicit def folioMonoid  = Monoid[Folio]
-  implicit def FolioIdHasEq = Eq.fromUniversalEquals[FolioId]
+  /** Price all the things. TODO: this feels like a repository */
+  def price[C: Monetary](id: AssetId): Money[MonetaryAmount, C] = ???
 
-  type Roster    = Map[Role, Set[EntityId]]
+  type Position = (AssetId, Quantity)
+  type Folio    = Map[AssetId, Quantity] // n.b algebraic relationship Position <=> Folio
+  type Trade    = Folio // `Trade` := `Folio` in motion
+  object Folio
+
+  def rollUp(ps: Position*): Folio = accumulate(ps.toList)
+  def price[C: Monetary](position: Position): Money[MonetaryAmount, C] = position match {
+    case (asset, quantity) => price[C](asset) * quantity
+  }
+
+  type FolioId = OpaqueId[Long, Folio]
+  implicit lazy val folioMonoid = Monoid[Folio]
+  implicit def FolioIdHasEq     = Eq.fromUniversalEquals[FolioId]
+
+  /** Note that Roster encodes the notion of a `CapitalKey` – per `Role`! */
+  // type Roster = Map[Role, Map[EntityId, Quantity]] FIXME make this work
+  type Roster = Map[Role, Set[EntityId]]
+  object Roster {
+    def empty: Roster = Map.empty
+  }
+
   type Account   = Map[FolioId, (Folio, Roster)]
   type AccountId = OpaqueId[Long, Account]
-  implicit def AccountIdHasEq = Eq.fromUniversalEquals[AccountId]
+  object Account
 
-// n.b the algebraic relationship between Position and Folio
+  implicit def AccountIdHasEq     = Eq.fromUniversalEquals[AccountId]
+  implicit lazy val accountMonoid = Monoid[Account]
 
+  /** (The) Ledger. I know what you're thinking: `LedgerId` ftw! But no. There's only one Ledger™ */
   type Ledger = Map[AccountId, Account]
+  object Ledger {
 
-  type EntityRoles  = Map[EntityId, Map[AccountId, Set[Role]]]
-  type AccountRoles = Map[AccountId, Map[EntityId, Set[Role]]]
+    def recorded(ledger: Ledger)(ao: AllocatedOrder): Ledger = ao match {
+      case (accountId, folioMap) =>
+        folioMap.toList foldMap {
+          case (folioId, tradeFolio) =>
+            (ledger(accountId) get folioId).fold(
+              ledger + (accountId -> Map(folioId -> ((tradeFolio, Roster.empty))))
+            ) {
+              case (folio, roster) =>
+                val updatedPositions = ((folio |+| tradeFolio), roster)
+                ledger + (accountId -> Map(folioId -> updatedPositions))
+            }
+        }
+    }
+  }
 
   type Allocation = Map[FolioId, Quantity] // must sum to 1... TODO typesafe normalization?
-  type Order      = (AccountId, Allocation, Folio)
-
-  type Execution = Nothing // Yet
-
-  type Transaction = Folio
-  // `Transaction` := `Folio` in motion
-  // `Folio` := `Transaction` at rest
-  // both are just sets of Positions, really
-
-  def index[K, V](kvs: Traversable[(K, V)]): Map[K, Traversable[V]] =
-    kvs groupBy (_._1) map {
-      case (k, kvs) => (k, kvs map (_._2))
-    }
-
-  def accumulate[K, V: Monoid](kvs: List[(K, V)]): Map[K, V] =
-    for {
-      (k, kvs) <- kvs groupBy (_._1)
-    } yield (k, kvs foldMap (_._2))
-
   object Allocation {
 
     lazy val Q = Financial[Quantity]
     import Q._
 
+    def allocate(allocation: Allocation)(order: Order): AllocatedOrder = ???
+
     def pariPassu(fids: Set[FolioId]): Allocation = {
       val n = fids.size
       (fids.toList zip List.fill(n)(Quantity.One / (Q fromInt n))).toMap
     }
-    def proRata(totalShares: Long)(capTable: Map[FolioId, Long]): Map[FolioId, Quantity] =
+    def proRata(totalShares: Long)(capTable: Map[FolioId, Long]): Allocation =
       capTable map {
         case (folioId, shares) =>
           val mySlice  = Q fromLong shares
@@ -139,48 +176,45 @@ abstract class Api[MonetaryAmount: Financial, Quantity: Financial] {
     }
   }
 
-  // TODO: `Folio`, `Account` objects with apply / unapply methods
-
-  type OrderId        = OpaqueId[Long, Order]
-  type QuotedOrder[C] = (Transaction, Money[MonetaryAmount, C])
-
-  def record(o: Order, ledger: Ledger): Ledger = o match {
-    case (accountId, Allocation.Single(folioId), transaction) =>
-      ledger(accountId)(folioId) match {
-        case (folio, roster) =>
-          val updated = ((folio |+| transaction), roster)
-          ledger + (accountId -> Map(folioId -> updated)) // FIXME: + vs |+|...
-      }
-    case _ => ??? // FIXME
-  }
-
-  // TODO: this feels like a repository
-  def price[C: Monetary](id: AssetId): Money[MonetaryAmount, C] = ???
-
-  def price[C: Monetary](position: Position): Money[MonetaryAmount, C] = position match {
-    case (asset, quantity) => price[C](asset) * quantity
-  }
-
-  implicit def qoMonoid[C: Monetary] = Monoid[QuotedOrder[C]]
-
-  /** Holds constants (stable values) useful for pattern matching */
-  object Quantity {
-    val Zero: Quantity = Financial[Quantity].zero
-    val One: Quantity  = Financial[Quantity].one
-  }
-
+  type Order   = (AccountId, Folio)
+  type OrderId = OpaqueId[Long, Order]
   object Order {
     import io.deftrade.money.Monetary.USD
-    def legacy(bd: BigDecimal, q: Long): QuotedOrder[USD] =
+    def legacy(bd: BigDecimal, q: Long): PricedTrade[USD] =
       (
         Map(LongId.reserved -> (Financial[Quantity] fromBigDecimal BigDecimal(q))),
         USD(Financial[MonetaryAmount] fromBigDecimal bd)
       )
   }
 
+  type AllocatedOrder = (AccountId, Map[FolioId, Folio])
+
+  // TODO: type constructors all the way?
+  // how to capture the fact that `[CCY: Monetary]`
+  type Execution[CCY]   = (OrderId, LocalDateTime, PricedTrade[CCY])
+  type ExecutionId[CCY] = OpaqueId[Long, Execution[CCY]]
+
+  type PricedTrade[CCY] = (Trade, Money[MonetaryAmount, CCY])
+  implicit def qoMonoid[C: Monetary] = Monoid[PricedTrade[C]]
+
+  type EntityRoles  = Map[EntityId, Map[AccountId, Set[Role]]]
+  type AccountRoles = Map[AccountId, Map[EntityId, Set[Role]]]
+
+  def index[K, V](kvs: Traversable[(K, V)]): Map[K, Traversable[V]] =
+    kvs groupBy (_._1) map {
+      case (k, kvs) => (k, kvs map (_._2))
+    }
+
+  /** N.B. that `accumulate` requires a List parameter; `index` does not. */
+  def accumulate[K, V: Monoid](kvs: List[(K, V)]): Map[K, V] =
+    for {
+      (k, kvs) <- kvs groupBy (_._1)
+    } yield (k, kvs foldMap (_._2))
 }
 
 object impl {
+
+  private[model] sealed trait Id[T] extends Any { def id: T }
 
   final case class OpaqueId[T, P](val id: T) extends AnyVal with Id[T]
 
