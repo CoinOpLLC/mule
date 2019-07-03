72d28603ecda1bf63d5b42c65b4b2d2ae813d6f8
ndwade <nicholas.d.wade@gmail.com>
2019-07-03 07:28:48 -0700

    model.EntityAccountMapping has interesting problem...

--- modules/model/src/main/scala/io/deftrade/model/EntityAccountMapping.scala
+++ modules/model/src/main/scala/io/deftrade/model/EntityAccountMapping.scala
@@ -1,12 +1,7 @@
 package io.deftrade
 package model
 
-import money._
-import time.{ LocalDate }
-
-import kves._
-
-import repos._
+import money._, time._, kves._, repos._, refinements._
 
 import enumeratum._
 
@@ -15,9 +10,11 @@ import cats.implicits._
 import cats.data.{ NonEmptyMap, NonEmptySet }
 
 import eu.timepit.refined
-// import refined.{ cats => refinedCats, _ }
+import refined.W
 import refined.api.Refined
+import refined.boolean.AnyOf
 import refined.numeric._
+// import refined.{ cats => refinedCats, _ }
 
 import io.circe.Json
 
@@ -26,60 +23,26 @@ import io.circe.Json
   * See Also: `model.Role`s.
   */
 sealed trait LegalEntity extends Product with Serializable {
-  def name: LegalEntity.IsVarChar
-  def dtid: LegalEntity.DTID
+  def name: VarChar
+  def dtid: LegalEntity.Dtid
   def meta: Json
 }
 
 /** `LegalEntity`s recognized by the system. */
 object LegalEntity extends WithKey[Long, LegalEntity] {
 
-  /**
-    * RDB friendly `String`s that are born usable as is.
-    * Defaults to Postgres, which is the shorter limit (126)
-    */
-  type IsVarChar = IsVarChar126
-
-  /** Postgres optimizes strings less than this. */
-  type IsVarChar126 = String Refined refinements.IsVarChar126
-
-  /** Typical SQL */
-  type IsVarChar255 = String Refined refinements.IsVarChar255
-
-  /**
-    * Post Randomization IsSSN validation: i.e., cursory only.
-    * See also:
-    * https://en.wikipedia.org/wiki/Social_Security_number#Valid_SSNs
-    * https://www.ssa.gov/employer/randomization.html
-    * https://www.ssa.gov/history/ssn/geocard.html
-    */
-  type IsSSN = String Refined refinements.IsSSN
-
-  /**
-    * An `LegalEntity` represents a legal (e.g. corporate, or non-profit) body.
-    * TODO: refine (no pun intended) the requirements on US EINs.
-    * TODO: Internationalize with an ADT.
-    */
-  type IsEIN = String Refined refinements.IsEIN
-
-  /**
-    * An algo in my head told me to do this.
-    */
-  type IsAIN = String Refined refinements.IsAIN
-
-  import refined.boolean.Or
-  lazy val rf = refinements
-  // type DTID = String Refined Or[rf.IsSSN, rf.IsEIN Or rf.IsAIN] // adding another Or kills auto derivation
-  type DTID = String Refined Or[rf.IsSSN, rf.IsEIN] // adding another Or kills auto derivation
-
+  import shapeless.{ ::, HNil }
   import refined.auto._
+
+  /** Domain Typed Id */
+  type Dtid = String Refined AnyOf[IsSsn :: IsEin :: IsAin :: HNil]
 
   /**
     *`NaturalPerson`s are people. Also, `NaturalPerson`s are `LegalEntity`s.
     */
   final case class NaturalPerson(
-      name: IsVarChar,
-      ssn: IsSSN,
+      name: VarChar,
+      ssn: Ssn,
       dob: LocalDate,
       meta: Json
   ) extends LegalEntity {
@@ -89,13 +52,13 @@ object LegalEntity extends WithKey[Long, LegalEntity] {
   /**
     * `Corporation`s are `LegalEntity`s too!
     */
-  final case class Corporation(name: IsVarChar, ein: IsEIN, meta: Json) extends LegalEntity {
+  final case class Corporation(name: VarChar, ein: Ein, meta: Json) extends LegalEntity {
     def dtid = ein
   }
 
   /** TODO flesh this concept out... minimum viable PM */
-  final case class Algorithm(name: IsVarChar, ain: IsAIN, meta: Json) extends LegalEntity {
-    def dtid = ??? // ain FIXME
+  final case class Algorithm(name: VarChar, ain: Ain, meta: Json) extends LegalEntity {
+    def dtid = ain
   }
 
   implicit def eqEntity = Eq.fromUniversalEquals[LegalEntity]
@@ -248,8 +211,11 @@ abstract class EntityAccountMapping[Q: Financial] extends Ledger[Q] { self =>
     */
   case class Account(roster: Roster, folioKey: Folio.Key)
   object Account {
-    import refinements.ValidRange
+
+    type ValidRange = Interval.Closed[W.`100000100100L`.T, W.`999999999999L`.T]
+
     implicit def eqValidRangeHackHackHack: Eq[ValidRange] = Eq.fromUniversalEquals[ValidRange]
+
     type Key = Long Refined ValidRange // remember you only get one free (value class) wrapper
     object Key {
       implicit def orderAccountId: cats.Order[Key] = cats.Order by { _.value }
@@ -279,111 +245,4 @@ abstract class EntityAccountMapping[Q: Financial] extends Ledger[Q] { self =>
   type FolioAuth             = (Folio.Key, AccountAuth)
   type FolioAuths            = (FolioAuth, FolioAuth)
   type AuthorizedTransaction = (Transaction, FolioAuths)
-}
-
-object refinements {
-
-  import refined.W
-  import refined.boolean.{ And }
-  import refined.collection.{ MaxSize, NonEmpty }
-  import refined.string.{ MatchesRegex, Trimmed }
-  import refined.char.{ Digit }
-
-  import refined.api.Validate
-
-  import shapeless.syntax.singleton._
-
-  type ValidRange = Interval.Closed[W.`100000100100L`.T, W.`999999999999L`.T]
-
-  type IsVarChar126 = Trimmed And MaxSize[W.`126`.T]
-  type IsVarChar255 = Trimmed And MaxSize[W.`255`.T]
-
-  final case class IsSSN private ()
-  object IsSSN {
-
-    implicit def ssnValidate: Validate.Plain[String, IsSSN] =
-      Validate.fromPredicate(predicate, t => s"$t is certainly not a valid IsSSN", IsSSN())
-
-    private val regex = """^(\d{3})-(\d{2})-(\d{4})$""".r.pattern
-    private val predicate: String => Boolean = s => {
-      val matcher = regex matcher s
-      matcher.find() && matcher.matches() && {
-        import matcher.group
-        val an      = group(1).toInt
-        def gn      = group(2).toInt
-        def sn      = group(3).toInt
-        def checkAn = 0 < an && an != 666 /* sic */ && an < 900
-        checkAn && 0 < gn && 0 < sn
-      }
-    }
-  }
-
-  import shapeless.syntax.singleton._
-
-  object IsEIN extends RxPredicate("[0-9]{2}-[0-9]{7}".narrow)
-  type IsEIN = IsEIN.MRx
-
-  type IsAIN = IsAIN.MRx
-  object IsAIN extends RxPredicate("666-[A-F]{2}-[0-9]{6}".narrow)
-
-  private[model] sealed abstract class RxPredicate[P](p: P) {
-    type Pattern = P // =:= p.T
-    val Pattern = p
-    final type MRx = MatchesRegex[Pattern]
-  }
-}
-
-object IsinScratch {
-  import refined.boolean.{ And }
-  import refined.collection.{ MaxSize, NonEmpty }
-  import refined.string.{ MatchesRegex, Trimmed }
-  import refined.char.{ Digit }
-
-  import refined.api.Validate
-
-  import shapeless.syntax.singleton._
-  import scala.util.Try
-
-  val isins = List(
-    "US0378331005",
-    "US0373831005",
-    "U50378331005",
-    "US03378331005",
-    "AU0000XVGZA3",
-    "AU0000VXGZA3",
-    "FR0000988040"
-  )
-
-  type IsIsin       = MatchesRxIsin And LuhnyIsin
-  type StringIsIsin = String Refined IsIsin
-
-  val isinRx = """[A-Z]{2}[A-Z0-9]{9}\d""".witness
-  type MatchesRxIsin = MatchesRegex[isinRx.T]
-
-  sealed abstract case class LuhnyIsin()
-  object LuhnyIsin {
-
-    implicit def isinValidate: Validate.Plain[String, LuhnyIsin] =
-      Validate fromPredicate (predicate, t => s"$t is not Luhny", new LuhnyIsin() {})
-
-    private def predicate(isin: String): Boolean =
-      Try {
-
-        val digits = for {
-          c <- isin.trim.toUpperCase
-          d <- Character.digit(c, 36).toString
-        } yield d.asDigit
-
-        val checksum = digits.reverse.zipWithIndex.foldLeft(0) {
-          case (sum, (digit, i)) =>
-            if (i % 2 == 0)
-              sum + digit
-            else
-              sum + (digit * 2) / 10 + (digit * 2) % 10
-        }
-        checksum % 10 === 0
-
-      } getOrElse false
-  }
-
 }
