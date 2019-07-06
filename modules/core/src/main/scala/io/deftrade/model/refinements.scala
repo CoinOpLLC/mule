26e722b9e3893144d0de2677a7b3df9083e041c7
ndwade <nicholas.d.wade@gmail.com>
2019-07-06 06:02:16 -0700

    fixed the instrument id mess

---
+++ modules/core/src/main/scala/io/deftrade/model/refinements.scala
@@ -0,0 +1,243 @@
+package io.deftrade
+package model
+
+import money._
+
+import cats.implicits._
+
+import eu.timepit.refined
+import refined.api.Refined
+import refined.W
+import refined.boolean.{ And, Or }
+import refined.collection.{ MaxSize }
+import refined.string.{ MatchesRegex, Trimmed }
+import refined.api.Validate
+
+import shapeless.syntax.singleton._
+
+/**
+  * A palette of domain specific refined types.
+  *
+  * Prêt à porter, as it were.
+  */
+object refinements {
+
+  /**
+    * RDB friendly `String`s that are born usable as is.
+    * Defaults to Postgres, which is the shorter limit (126)
+    */
+  type IsVarChar = IsVarChar126
+  type VarChar   = String Refined IsVarChar
+
+  /** Postgres optimizes strings less than this. */
+  type IsVarChar126 = Trimmed And MaxSize[W.`126`.T]
+  type VarChar126   = String Refined IsVarChar126
+
+  /** Typical SQL */
+  type IsVarChar255 = Trimmed And MaxSize[W.`255`.T]
+  type VarChar255   = String Refined IsVarChar255
+
+  final val MatchesRxSsn = """\d{3}-\d{2}-\d{4}""".witness
+  type MatchesRxSsn = MatchesRxSsn.T
+
+  /**
+    * Post "Randomization" SSN validation: i.e., cursory only.
+    * See also:
+    * https://en.wikipedia.org/wiki/Social_Security_number#Valid_SSNs
+    * https://www.ssa.gov/employer/randomization.html
+    * https://www.ssa.gov/history/ssn/geocard.html
+    */
+  sealed abstract case class CheckedSsn private ()
+  object CheckedSsn {
+
+    lazy val instance: CheckedSsn = new CheckedSsn() {}
+
+    implicit def ssnValidate: Validate.Plain[String, CheckedSsn] =
+      Validate.fromPredicate(predicate, t => s"$t is certainly not a valid IsSsn", instance)
+
+    private val predicate: String => Boolean = s => {
+      scala.util.Try {
+        val an :: gn :: sn :: Nil = (s split '-' map (_.toInt)).toList
+        def checkAn               = 0 < an && an != 666 /* sic */ && an < 900
+        checkAn && 0 < gn && 0 < sn
+      } getOrElse false
+    }
+  }
+
+  type IsSsn = MatchesRxSsn And CheckedSsn
+  type Ssn   = String Refined IsSsn
+
+  /**
+    * An `LegalEntity` represents a legal (e.g. corporate, or non-profit) body.
+    * TODO: refine (pun intended) the requirements on US EINs.
+    * TODO: Internationalize with an ADT.
+    */
+  val IsEin = """\d{2}-\d{7}""".witness
+  type IsEin = MatchesRegex[IsEin.T]
+  type Ein   = String Refined IsEin
+
+  /**
+    * An ISIN is a twelve character string that must match a certain regex, and whose characters
+    * must pass a certain (Luhn) checksum.
+    */
+  val MatchesRxIsin = """[A-Z]{2}[A-Z0-9]{9}[0-9]""".witness
+  type MatchesRxIsin = MatchesRegex[MatchesRxIsin.T]
+
+  sealed abstract case class CheckedIsin()
+  object CheckedIsin {
+
+    lazy val instance: CheckedIsin = new CheckedIsin() {}
+
+    implicit def isinValidate: Validate.Plain[String, CheckedIsin] =
+      Validate fromPredicate (predicate, t => s"$t is not Luhny", instance)
+
+    private def predicate(isin: String): Boolean =
+      /**
+        * TODO need to add country checks,
+        and break them out into a separate function
+
+        - green-light only a predefined list of juristictions for registered securities
+        - two-letter code mappings reserved for "users" are adopted by deftrade:
+            - ZZ: unregistered securities with house-issued numbers.
+            - XB: Interactive Brokers `ConId` number
+        - the other 25 mappings in X[A-Z] are reserved for use facing other brokers' apis.
+        */
+      failsafe {
+
+        val digits = for {
+          c <- isin
+          d <- Character.digit(c, 36).toString
+        } yield d.asDigit
+
+        val check = for ((d, i) <- digits.reverse.zipWithIndex) yield luhn(d, i)
+
+        check.sum % 10 === 0
+
+      }
+  }
+
+  /** Psin: Pseudo Isin: matches regex, but uses the 9 digit body for proprietary mappings. */
+  sealed abstract case class CheckedPsin()
+  object CheckedPsin {
+
+    lazy val instance: CheckedPsin = new CheckedPsin() {}
+
+    implicit def isinValidate: Validate.Plain[String, CheckedPsin] =
+      Validate fromPredicate (predicate, t => s"$t is not Luhny", instance)
+
+    /** FIXME  factor the shit out of this */
+    private def predicate(isin: String): Boolean =
+      /**
+        * TODO need to add country checks,
+        and break them out into a separate function
+
+        - green-light only a predefined list of juristictions for registered securities
+        - two-letter code mappings reserved for "users" are adopted by deftrade:
+            - ZZ: unregistered securities with house-issued numbers.
+            - XB: Interactive Brokers `ConId` number
+        - the other 25 mappings in X[A-Z] are reserved for use facing other brokers' apis.
+        */
+      failsafe {
+
+        val digits = for {
+          c <- isin
+          d <- Character.digit(c, 36).toString
+        } yield d.asDigit
+
+        val check = for ((d, i) <- digits.reverse.zipWithIndex) yield luhn(d, i)
+
+        check.sum % 10 === 0
+
+      }
+  }
+
+  type IsIsin = MatchesRxIsin And CheckedIsin
+  type Isin   = String Refined IsIsin
+
+  type IsPsin = MatchesRxIsin And CheckedPsin // sic
+  type Psin   = String Refined IsPsin
+
+  /**
+    * How deftrade canonicalizes securities identifiers:
+    * Use `Isin`s where non-isin uses use reserved country codes: {X*, ZZ}
+    *
+    * Universal Security Identifying Number: Usin
+    */
+  type IsUsin = IsIsin Or IsPsin
+  type Usin   = String Refined IsUsin
+
+  object Usin {
+
+    /**
+      * the least we can do
+      */
+    def from(s: String): Result[Usin] = ???
+
+    def fromIsin(isin: Isin): Isin    = ???
+    def fromCusip(cusip: Cusip): Isin = ???
+    // def fromSedol(sedol: Sedol): Isin = ???
+
+    def fromUnreg(unreg: Unreg): Psin = ???
+    def fromIbrk(ibrk: Ibrk): Psin    = ???
+
+    def toIsin(usin: Usin): Result[Isin]   = ???
+    def toCusip(usin: Usin): Result[Cusip] = ???
+    // def toSedol(usin: Usin): Result[Sedol] = ???
+
+    def toUnreg(usin: Usin): Result[Unreg] = ???
+    def toIbrk(usin: Usin): Result[Ibrk]   = ???
+  }
+
+  /**
+    * A CUSIP is a nine character string that must match a certain regex, and whose characters
+    * must pass a certain (Luhn) checksum.
+    */
+  sealed abstract case class CheckedCusip()
+  object CheckedCusip {
+
+    lazy val instance: CheckedCusip = new CheckedCusip() {}
+
+    implicit def cusipValidate: Validate.Plain[String, CheckedCusip] =
+      Validate fromPredicate (predicate, t => s"$t is not legit", instance)
+
+    private def predicate(isin: String): Boolean =
+      failsafe {
+
+        /** FIXME flesh this out */
+        true
+
+      }
+  }
+
+  val MatchesRxCusip = """[0-9]{3}[0-9A-Z]{3}[0-9]{3}""".witness
+  type MatchesRxCusip = MatchesRegex[MatchesRxCusip.T]
+
+  type IsCusip = MatchesRxCusip And CheckedCusip
+  type Cusip   = String Refined IsCusip
+
+  /**
+    * `Ibrk` identifiers represent **Interactive Brokers**
+    * `ConId`'s
+    */
+  val MatchesRxIbrk = """\d{8}\d?""".witness // 8 or 9 char, all numbers (evidently)
+  type MatchesRxIbrk = MatchesRegex[MatchesRxIbrk.T]
+  type IsIbrk        = MatchesRxIbrk
+  type Ibrk          = String Refined IsIbrk
+
+  /**
+    * `Unreg` identifiers represent unregistered securities, numbered by the firm.
+    */
+  val MatchesRxUnreg = """\d{8}\d?""".witness // 8 or 9 char, all numbers (evidently)
+  type MatchesRxUnreg = MatchesRegex[MatchesRxUnreg.T]
+  type IsUnreg        = MatchesRxUnreg
+  type Unreg          = String Refined IsUnreg
+
+  //------
+
+  // https://en.wikipedia.org/wiki/Luhn_algorithm
+  private def luhn(digit: Int, idx: Int): Int =
+    if (idx % 2 === 0) digit else (digit * 2) / 10 + (digit * 2) % 10
+
+  private def failsafe(predcomp: => Boolean): Boolean = scala.util.Try(predcomp) getOrElse false
+
+}
--- modules/model/src/main/scala/io/deftrade/model/instruments.scala
+++ modules/model/src/main/scala/io/deftrade/model/instruments.scala
@@ -2,84 +2,82 @@ package io.deftrade
 package model
 package keys
 
-import io.deftrade.model.refinements.Isin
+import io.deftrade.model.refinements.{ Isin, Psin, Usin }
 
 import cats.{ Order }
 import cats.instances.string._
-
-import scala.util.matching.Regex
 
 sealed trait Derivative { self: InstrumentIdentifier =>
   def underlyer: InstrumentIdentifier
 }
 object Derivative
 
-sealed trait InstrumentIdentifier { def isin: Isin }
+sealed trait InstrumentIdentifier { def usin: Usin }
 
 object InstrumentIdentifier {
 
   // A FixedCouponBond or CapitalIndexedBond.
-  case class Bond(val isin: Isin) extends InstrumentIdentifier
+  case class Bond(val usin: Usin) extends InstrumentIdentifier
   // A BondFuture.
-  case class BondFuture(val isin: Isin) extends InstrumentIdentifier
+  case class BondFuture(val usin: Usin) extends InstrumentIdentifier
   // A BondFutureOption.
-  case class BondFutureOption(val isin: Isin) extends InstrumentIdentifier
+  case class BondFutureOption(val usin: Usin) extends InstrumentIdentifier
   // A BulletPayment.
-  case class BulletPayment(val isin: Isin) extends InstrumentIdentifier
+  case class BulletPayment(val usin: Usin) extends InstrumentIdentifier
   // A product only used for calibration.
-  case class Calibration(val isin: Isin) extends InstrumentIdentifier
+  case class Calibration(val usin: Usin) extends InstrumentIdentifier
   // CreditAccount Default Swap (CDS)
-  case class Cds(val isin: Isin) extends InstrumentIdentifier
+  case class Cds(val usin: Usin) extends InstrumentIdentifier
   // CDS index
-  case class CdsIndex(val isin: Isin) extends InstrumentIdentifier
+  case class CdsIndex(val usin: Usin) extends InstrumentIdentifier
   // Constant Maturity Swap (CMS)
-  case class Cms(val isin: Isin) extends InstrumentIdentifier
+  case class Cms(val usin: Usin) extends InstrumentIdentifier
   // A Deliverable Swap Forward
   // https://www.cmegroup.com/trading/interest-rates/files/understanding-dsf.pdf
-  case class Dsf(val isin: Isin) extends InstrumentIdentifier
+  case class Dsf(val usin: Usin) extends InstrumentIdentifier
   // Exchange Traded Derivative - Future (ETD)
-  case class EtdFuture(val isin: Isin) extends InstrumentIdentifier // FIXME: this conflicts wtih mine...
+  case class EtdFuture(val usin: Usin) extends InstrumentIdentifier // FIXME: this conflicts wtih mine...
   // Exchange Traded Derivative - Option (ETD)
-  case class EtdOption(val isin: Isin) extends InstrumentIdentifier
+  case class EtdOption(val usin: Usin) extends InstrumentIdentifier
   // Forward Rate Agreement
-  case class Fra(val isin: Isin) extends InstrumentIdentifier
+  case class Fra(val usin: Usin) extends InstrumentIdentifier
   // FX Non-Deliverable Forward
-  case class FxNdf(val isin: Isin) extends InstrumentIdentifier
+  case class FxNdf(val usin: Usin) extends InstrumentIdentifier
   // A FxSingle.
-  case class FxSingle(val isin: Isin) extends InstrumentIdentifier
+  case class FxSingle(val usin: Usin) extends InstrumentIdentifier
   // A FxSingleBarrierOption.
-  case class FxSingleBarrierOption(val isin: Isin) extends InstrumentIdentifier
+  case class FxSingleBarrierOption(val usin: Usin) extends InstrumentIdentifier
   // A FxSwap.
-  case class FxSwap(val isin: Isin) extends InstrumentIdentifier
+  case class FxSwap(val usin: Usin) extends InstrumentIdentifier
   // A FxVanillaOption.
-  case class FxVanillaOption(val isin: Isin, val underlyer: InstrumentIdentifier) extends InstrumentIdentifier with Derivative
+  case class FxVanillaOption(val usin: Usin, val underlyer: InstrumentIdentifier) extends InstrumentIdentifier with Derivative
   // A IborCapFloor.
-  case class IborCapFloor(val isin: Isin) extends InstrumentIdentifier
+  case class IborCapFloor(val usin: Usin) extends InstrumentIdentifier
   // A IborFuture.
-  case class IborFuture(val isin: Isin) extends InstrumentIdentifier
+  case class IborFuture(val usin: Usin) extends InstrumentIdentifier
   // A IborFutureOption.
-  case class IborFutureOption(val isin: Isin) extends InstrumentIdentifier
+  case class IborFutureOption(val usin: Usin) extends InstrumentIdentifier
   // // A representation based on sensitivities.
-  case class Sensitivities(val isin: Isin) extends InstrumentIdentifier
+  case class Sensitivities(val usin: Usin) extends InstrumentIdentifier
   // A Swap.
-  case class Swap(val isin: Isin) extends InstrumentIdentifier
+  case class Swap(val usin: Usin) extends InstrumentIdentifier
   // A Swaption.
-  case class Swaption(val isin: Isin) extends InstrumentIdentifier
+  case class Swaption(val usin: Usin) extends InstrumentIdentifier
   // A TermDeposit.
-  case class TermDeposit(val isin: Isin) extends InstrumentIdentifier
+  case class TermDeposit(val usin: Usin) extends InstrumentIdentifier
 
-  case class AmortizingLoan(val isin: Isin)  extends InstrumentIdentifier
-  case class ConvertibleLoan(val isin: Isin) extends InstrumentIdentifier
+  case class AmortizingLoan(val usin: Usin)  extends InstrumentIdentifier
+  case class ConvertibleLoan(val usin: Usin) extends InstrumentIdentifier
 
-  case class CommonStock(val isin: Isin)    extends InstrumentIdentifier
-  case class PreferredStock(val isin: Isin) extends InstrumentIdentifier
+  case class CommonStock(val usin: Usin)    extends InstrumentIdentifier
+  case class PreferredStock(val usin: Usin) extends InstrumentIdentifier
 
-  case class StockIndexFutureOption(val isin: Isin, val underlyer: InstrumentIdentifier) extends InstrumentIdentifier with Derivative
-  case class StockIndexOption(val isin: Isin, val underlyer: InstrumentIdentifier)       extends InstrumentIdentifier with Derivative
-  case class StockIndexFuture(val isin: Isin, val underlyer: InstrumentIdentifier)       extends InstrumentIdentifier with Derivative
-  case class StockOption(val isin: Isin, val underlyer: InstrumentIdentifier)            extends InstrumentIdentifier with Derivative
+  case class StockIndexFutureOption(val usin: Usin, val underlyer: InstrumentIdentifier) extends InstrumentIdentifier with Derivative
+  case class StockIndexOption(val usin: Usin, val underlyer: InstrumentIdentifier)       extends InstrumentIdentifier with Derivative
+  case class StockIndexFuture(val usin: Usin, val underlyer: InstrumentIdentifier)       extends InstrumentIdentifier with Derivative
+  case class StockOption(val usin: Usin, val underlyer: InstrumentIdentifier)            extends InstrumentIdentifier with Derivative
 
-  implicit lazy val order: Order[InstrumentIdentifier] = Order by (_.isin.value)
+  implicit lazy val order: Order[InstrumentIdentifier] = Order by (_.usin.value)
 }
 
 import io.deftrade.money.Financial
