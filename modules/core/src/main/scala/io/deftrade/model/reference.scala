56c902f198a6673605d592b9e074e74d9e88510b
ndwade <nick@coinopllc.com>
2019-10-02 07:09:59 -0700

    made model.reference a package

--- modules/core/src/main/scala/io/deftrade/model/reference.scala
+++ modules/core/src/main/scala/io/deftrade/model/reference.scala
@@ -18,13 +18,157 @@ import shapeless.syntax.singleton._
 import scala.util.Try
 
 /** */
-object reference {
+package object reference {
 
   /** */
   final val MatchesRxSsn = """\d{3}-\d{2}-\d{4}""".witness
 
   /** */
   type MatchesRxSsn = MatchesRxSsn.T
+
+  /** */
+  type IsSsn = MatchesRxSsn And CheckedSsn
+
+  /** */
+  type Ssn = String Refined IsSsn
+
+  /**
+    * An `LegalEntity` represents a legal (eg corporate, or non-profit) body.
+    * TODO: refine (pun intended) the requirements on US EINs.
+    * TODO: Internationalize with an ADT.
+    */
+  val IsEin = """\d{2}-\d{7}""".witness
+
+  /** */
+  type IsEin = MatchesRegex[IsEin.T]
+
+  /** */
+  type Ein = String Refined IsEin
+
+  /** */
+  val Mic = """[A-Z]{3,4}""".witness
+
+  /** */
+  type Mic = String Refined MatchesRegex[Mic.T] // market venue
+
+  /**
+    * An ISIN is a twelve character string that must match a certain regex, and whose characters
+    * must pass a certain (Luhn) checksum.
+    */
+  val MatchesRxIsin = """[A-Z]{2}[A-Z0-9]{9}[0-9]""".witness
+
+  /** */
+  type MatchesRxIsin = MatchesRegex[MatchesRxIsin.T]
+
+  /** */
+  type IsIsin = MatchesRxIsin And CheckedIsin
+
+  /** */
+  type Isin = String Refined IsIsin
+
+  /** */
+  type IsPsin = MatchesRxIsin And CheckedPsin // sic
+  /** */
+  type Psin = String Refined IsPsin
+
+  /**
+    * How deftrade canonicalizes securities identifiers:
+    * Use `Isin`s where non-isin uses use reserved country codes: {X*, ZZ}
+    *
+    * Universal Security Identifying Number: Usin
+    */
+  type IsUsin = IsIsin Or IsPsin
+
+  /** */
+  type Usin = String Refined IsUsin
+
+  /** */
+  object Usin {
+
+    /**
+      * the least we can do
+      */
+    def from(s: String): Result[Usin] = ???
+
+    /** */
+    def fromIsin(isin: Isin): Isin = ???
+
+    /** */
+    def fromCusip(cusip: Cusip): Isin = ???
+    // def fromSedol(sedol: Sedol): Isin = ???
+
+    /** */
+    def fromUnreg(unreg: Unreg): Psin = ???
+
+    /** */
+    def fromIbrk(ibrk: Ibrk): Psin = ???
+
+    /** */
+    def toIsin(usin: Usin): Result[Isin] = ???
+
+    /** */
+    def toCusip(usin: Usin): Result[Cusip] = ???
+    // def toSedol(usin: Usin): Result[Sedol] = ???
+
+    /** */
+    def toUnreg(usin: Usin): Result[Unreg] = ???
+
+    /** */
+    def toIbrk(usin: Usin): Result[Ibrk] = ???
+  }
+
+  /** */
+  val MatchesRxCusip = """[0-9]{3}[0-9A-Z]{3}[0-9]{3}""".witness
+
+  /** */
+  type MatchesRxCusip = MatchesRegex[MatchesRxCusip.T]
+
+  /** */
+  type IsCusip = MatchesRxCusip And CheckedCusip
+
+  /** */
+  type Cusip = String Refined IsCusip
+
+  /**
+    * `Ibrk` identifiers represent [[https://interactivebrokers.com Interactive Brokers]]
+    * `ConId`'s
+    */
+  val MatchesRxIbrk = """\d{8}\d?""".witness // 8 or 9 char, all numbers (evidently)
+
+  /** */
+  type MatchesRxIbrk = MatchesRegex[MatchesRxIbrk.T]
+
+  /** */
+  type IsIbrk = MatchesRxIbrk
+
+  /** */
+  type Ibrk = String Refined IsIbrk
+
+  /**
+    * `Unreg` identifiers represent unregistered securities, numbered by the firm.
+    */
+  val MatchesRxUnreg = """\d{8}\d?""".witness // 8 or 9 char, all numbers (evidently)
+
+  /** */
+  type MatchesRxUnreg = MatchesRegex[MatchesRxUnreg.T]
+
+  /** */
+  type IsUnreg = MatchesRxUnreg
+
+  /** */
+  type Unreg = String Refined IsUnreg
+
+  // https://en.wikipedia.org/wiki/Luhn_algorithm
+  private[model] def luhn(digit: Int, idx: Int): Int =
+    if (idx % 2 === 0) digit else (digit * 2) / 10 + (digit * 2) % 10
+
+  private[model] def failsafe(thunk: => Boolean): Boolean = Try(thunk).fold(_ => false, identity)
+}
+
+/**
+  * Types and methods for financial reference data.
+  */
+package reference {
 
   /**
     * Post "Randomization" SSN validation: i.e., cursory only.
@@ -56,25 +200,6 @@ object reference {
   }
 
   /** */
-  type IsSsn = MatchesRxSsn And CheckedSsn
-
-  /** */
-  type Ssn = String Refined IsSsn
-
-  /**
-    * An `LegalEntity` represents a legal (eg corporate, or non-profit) body.
-    * TODO: refine (pun intended) the requirements on US EINs.
-    * TODO: Internationalize with an ADT.
-    */
-  val IsEin = """\d{2}-\d{7}""".witness
-
-  /** */
-  type IsEin = MatchesRegex[IsEin.T]
-
-  /** */
-  type Ein = String Refined IsEin
-
-  /** */
   sealed abstract case class CountryCode(
       alpha2: Alpha2,
       alpha3: Alpha3,
@@ -93,8 +218,8 @@ object reference {
   object CountryCode extends WithOpaqueKey[Int, CountryCode] {
 
     def regions: Map[Num3, VarChar]             = ???
-    def subRegions: Map[Num3, VarChar]          = ???
     def intermediateRegions: Map[Num3, VarChar] = ???
+    def subRegions: Map[Num3, VarChar]          = ???
 
     def apply(alpha2: Alpha2,
               alpha3: Alpha3,
@@ -116,23 +241,7 @@ object reference {
         intermediateRegion = (regions get countryCode).fold(VarChar.empty)(identity),
         // s"ISO 3166-2:$alpha2",
       ) {}
-
   }
-
-  /** */
-  val Mic = """[A-Z]{3,4}""".witness
-
-  /** */
-  type Mic = String Refined MatchesRegex[Mic.T] // market venue
-
-  /**
-    * An ISIN is a twelve character string that must match a certain regex, and whose characters
-    * must pass a certain (Luhn) checksum.
-    */
-  val MatchesRxIsin = """[A-Z]{2}[A-Z0-9]{9}[0-9]""".witness
-
-  /** */
-  type MatchesRxIsin = MatchesRegex[MatchesRxIsin.T]
 
   /** */
   sealed abstract case class CheckedIsin()
@@ -197,63 +306,6 @@ object reference {
     }
   }
 
-  /** */
-  type IsIsin = MatchesRxIsin And CheckedIsin
-
-  /** */
-  type Isin = String Refined IsIsin
-
-  /** */
-  type IsPsin = MatchesRxIsin And CheckedPsin // sic
-  /** */
-  type Psin = String Refined IsPsin
-
-  /**
-    * How deftrade canonicalizes securities identifiers:
-    * Use `Isin`s where non-isin uses use reserved country codes: {X*, ZZ}
-    *
-    * Universal Security Identifying Number: Usin
-    */
-  type IsUsin = IsIsin Or IsPsin
-
-  /** */
-  type Usin = String Refined IsUsin
-
-  /** */
-  object Usin {
-
-    /**
-      * the least we can do
-      */
-    def from(s: String): Result[Usin] = ???
-
-    /** */
-    def fromIsin(isin: Isin): Isin = ???
-
-    /** */
-    def fromCusip(cusip: Cusip): Isin = ???
-    // def fromSedol(sedol: Sedol): Isin = ???
-
-    /** */
-    def fromUnreg(unreg: Unreg): Psin = ???
-
-    /** */
-    def fromIbrk(ibrk: Ibrk): Psin = ???
-
-    /** */
-    def toIsin(usin: Usin): Result[Isin] = ???
-
-    /** */
-    def toCusip(usin: Usin): Result[Cusip] = ???
-    // def toSedol(usin: Usin): Result[Sedol] = ???
-
-    /** */
-    def toUnreg(usin: Usin): Result[Unreg] = ???
-
-    /** */
-    def toIbrk(usin: Usin): Result[Ibrk] = ???
-  }
-
   /**
     * A CUSIP is a nine character string that must match a certain regex, and whose characters
     * must pass a certain (Luhn) checksum.
@@ -279,54 +331,4 @@ object reference {
 
       }
   }
-
-  /** */
-  val MatchesRxCusip = """[0-9]{3}[0-9A-Z]{3}[0-9]{3}""".witness
-
-  /** */
-  type MatchesRxCusip = MatchesRegex[MatchesRxCusip.T]
-
-  /** */
-  type IsCusip = MatchesRxCusip And CheckedCusip
-
-  /** */
-  type Cusip = String Refined IsCusip
-
-  /**
-    * `Ibrk` identifiers represent [[https://interactivebrokers.com Interactive Brokers]]
-    * `ConId`'s
-    */
-  val MatchesRxIbrk = """\d{8}\d?""".witness // 8 or 9 char, all numbers (evidently)
-
-  /** */
-  type MatchesRxIbrk = MatchesRegex[MatchesRxIbrk.T]
-
-  /** */
-  type IsIbrk = MatchesRxIbrk
-
-  /** */
-  type Ibrk = String Refined IsIbrk
-
-  /**
-    * `Unreg` identifiers represent unregistered securities, numbered by the firm.
-    */
-  val MatchesRxUnreg = """\d{8}\d?""".witness // 8 or 9 char, all numbers (evidently)
-
-  /** */
-  type MatchesRxUnreg = MatchesRegex[MatchesRxUnreg.T]
-
-  /** */
-  type IsUnreg = MatchesRxUnreg
-
-  /** */
-  type Unreg = String Refined IsUnreg
-
-  //------
-
-  // https://en.wikipedia.org/wiki/Luhn_algorithm
-  private def luhn(digit: Int, idx: Int): Int =
-    if (idx % 2 === 0) digit else (digit * 2) / 10 + (digit * 2) % 10
-
-  private def failsafe(thunk: => Boolean): Boolean = Try(thunk).fold(_ => false, identity)
-
 }
