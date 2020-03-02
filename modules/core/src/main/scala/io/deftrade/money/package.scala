a14291c9442a0b212442c5f77c33937183008099
--global <--global>
2020-03-02 05:09:30 -0800

    comments only

--- modules/core/src/main/scala/io/deftrade/money/package.scala
+++ modules/core/src/main/scala/io/deftrade/money/package.scala
@@ -22,21 +22,26 @@ import cats.kernel.CommutativeGroup
   * Single-import DSL for monetary expressions, including currency support.
   *
   *   ==Inspiration:==
-  *   - [[https://www.joda.org/joda-money/ Joda Mny]] of course
-  *   - `squants.market`
+  *   - [[https://www.joda.org/joda-money/ Joda Money]] of course
+  *   - [[http://www.squants.com/ squants.market.Money]]
   *   - [[https://github.com/Appendium/objectlabkit ObjectLabKit]]
   *
   *   ==Implementation:==
-  *   - leverages java Currency support for [[https://en.wikipedia.org/wiki/ISO_4217 ISO 4217]]
-  *       - so JVM dependency
-  *   - [[Mny]] is a value class type constructor for type parameter `[N: Financial]`
-  *   - distinct types for each currency
-  *       - summon implicit [[Currency]]`[C`] typeclass instance given a currency type `C`
-  *   - abstract over currencies for single implicit `cats.CommutativeGroup` function
-  *   - dependencies (the usual suspects):
-  *       - `typelevel algebra` (which includes `spire`)
-  *       - `Enumeratum` to walk thru implemented currency codes
-  *       - integration with `Refined` via `RefType[F[_,_]]`
+  *   - leverages [[java.util.Currency]] support for
+  *     [[https://en.wikipedia.org/wiki/ISO_4217 ISO 4217]]
+  *       - incurs JVM dependency :(
+  *   - [[Mny]] is a value class type constructor with two type parameters:
+  *       - a numeric type `N` for which a `Financial[N]` instance is in implicit scope
+  *           - entails [[cats.kernel.CommutativeGroup]] and [[spire.math.Fractional]]
+  *       - a type `C` for which a [[Currency]]`[C]` instance is in implicit scope
+  *           - `C` distinct for each supported currency
+  *       - [[model.Money]][C] binds the `N` type in [[Mny]], leaving one parameter free
+  *         (the currency `C`)
+  *
+  *   TODO: Integration with [[eu.timepit.refined.api.Refined]] via `RefType[F[_,_]]`
+  *         was done, but abandonned due to lack
+  *         of apparent utility and evident implicit scope ambiguity.
+  *         Revisit if need be.
   *
   *   TODO: track evolving
   * [[https://en.wikipedia.org/wiki/ISO_4217#Cryptocurrencies crypto support]]
@@ -47,5 +52,4 @@ package object money {
   /** TODO: This will break down in corner cases! Review how other projects handle this. */
   implicit def financialCommutativeGroup[MA: Financial]: CommutativeGroup[MA] =
     Financial[MA].commutativeGroup
-
 }
